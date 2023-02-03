3f3c4052fecca09370fedec2d5c723acc74c3f2f
Glenn Renfro <grenfro@vmware.com>
2023-02-03 17:43:44 -0500

    Removed Water Level Data moduile.   Not needed
    We simplified the model so it is no longer needed

--- modulithbasedapp/src/main/java/io/spring/waterlevel/streamdata/StreamLevelData.java
+++ modulithbasedapp/src/main/java/io/spring/waterlevel/streamdata/StreamLevelData.java
@@ -1,9 +1,81 @@
 package io.spring.waterlevel.streamdata;
 
-import io.spring.waterlevel.measurement.WaterMeasurement;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
-public class StreamLevelData extends WaterMeasurement {
+public class StreamLevelData {
+    private String measurementKey;
+    private String sensorId;
 
+    private String name;
+    private ZonedDateTime dateCaptured;
+    private Float streamHeight;
+    private String status;
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm z");
+
+    public StreamLevelData(String stringData) {
+        String[] rawData = stringData.split("\t");
+        Assert.isTrue(rawData.length == 6, "There must be 6 fields in data.  There were " + rawData.length);
+        this.sensorId = rawData[1];
+        this.dateCaptured = ZonedDateTime.parse(rawData[2] + " " +rawData[3], formatter);
+        this.streamHeight = Float.valueOf(rawData[4]);
+        this.status = rawData[5];
+        measurementKey = this.sensorId + this.dateCaptured;
+    }
+
+    public StreamLevelData() {
+
+    }
+
+    public ZonedDateTime getDateCaptured() {
+        return dateCaptured;
+    }
+    public void setDateCaptured(ZonedDateTime dateCaptured) {
+        this.dateCaptured = dateCaptured;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public String getSensorId() {
+        return sensorId;
+    }
+    public void setSensorId(String sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String toString() {
+        return measurementKey + " " + sensorId + " " + this.dateCaptured + " " + this.streamHeight + " " + this.status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMeasurementKey() {
+        return measurementKey;
+    }
+
+    public void setMeasurementKey(String measurementKey) {
+        this.measurementKey = measurementKey;
+    }
+
+    public Float getStreamHeight() {
+        return streamHeight;
+    }
+
+    public void setStreamHeight(Float streamHeight) {
+        this.streamHeight = streamHeight;
+    }
 }

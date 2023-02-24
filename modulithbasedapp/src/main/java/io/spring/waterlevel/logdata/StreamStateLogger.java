bca19059cfc5ea84b9d18cc402e33ec97db8d87b
Glenn Renfro <grenfro@vmware.com>
2023-02-24 17:38:23 -0500

    Moving stubs to handle second part of the lab.

---
+++ modulithbasedapp/src/main/java/io/spring/waterlevel/logdata/StreamStateLogger.java
@@ -0,0 +1,12 @@
+package io.spring.waterlevel.logdata;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StreamStateLogger {
+
+    private static final Log log = LogFactory.getLog(StreamStateLogger.class);
+
+}

365eadf44a84882fc0113ccac716b532371d9e75
Glenn Renfro <grenfro@vmware.com>
2023-02-24 18:02:16 -0500

    Added ability to record stream status to a database.
    Previously we were just logging a message that this was a todo.

--- modulithbasedapp/src/main/java/io/spring/waterlevel/storestreamdata/StreamStateRecorder.java
+++ modulithbasedapp/src/main/java/io/spring/waterlevel/storestreamdata/StreamStateRecorder.java
@@ -3,13 +3,22 @@ package io.spring.waterlevel.storestreamdata;
 import io.spring.waterlevel.wateradvisor.StreamDataStatus;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.modulith.ApplicationModuleListener;
 import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
 
 @Service
 public class StreamStateRecorder {
 
     private static final Log log = LogFactory.getLog(StreamStateRecorder.class);
+
+    private DataSource dataSource;
+
+    public  StreamStateRecorder(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @ApplicationModuleListener
     void on(StreamDataStatus event) throws InterruptedException {
@@ -17,7 +26,10 @@ public class StreamStateRecorder {
         var sensorId = event.getSensorId();
 
         log.info("Received Stream Data Store Request " + sensorId);
-
+        JdbcTemplate template = new JdbcTemplate(this.dataSource);
+        template.update("INSERT INTO stream_status (sensor_id, status) VALUES(?, ?)",
+                event.getSensorId(), event.getStatus());
         log.info("Storing Stream Data for " + sensorId);
     }
+
 }
--- modulithbasedapp/src/main/resources/schema.sql
+++ modulithbasedapp/src/main/resources/schema.sql
@@ -1,6 +1,5 @@
-CREATE TABLE IF NOT EXISTS creek_status
+CREATE TABLE IF NOT EXISTS stream_status
 (
-    creek_measurement_key varchar(255) NOT NULL,
     sensor_id varchar(255),
     status varchar(1024)
 );

8630aa72c22acd98bcc8b459e664c5c11109178f
Glenn Renfro <grenfro@vmware.com>
2023-03-13 11:58:47 -0400

    Updated tests to use ApplicationModuleTest mode properly
    in this case we want to show that we can access direct dependencies from a module.
    
    And by removing the mode it will fail.

--- modulithbasedapp/src/test/java/io/spring/waterlevel/streamdata/StreamDataIntegrationTests.java
+++ modulithbasedapp/src/test/java/io/spring/waterlevel/streamdata/StreamDataIntegrationTests.java
@@ -1,6 +1,5 @@
 package io.spring.waterlevel.streamdata;
 
-import io.spring.waterlevel.logdata.StreamStateLogger;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.modulith.test.ApplicationModuleTest;
@@ -12,13 +11,6 @@ class StreamDataIntegrationTests {
 
     @Autowired
     StreamDataService streamDataService;
-
-    /**
-     * This will not autowire because the integration test defaults to `STANDALONE` which means it will only focus on the current module.
-     * If you needed it you would have to set the bootstrap mode to "ALL_DEPENDENCIES".  And that maybe a signal for tight coupling between modules.
-     */
-//    @Autowired
-//    StreamStateLogger streamStateLogger;
 
     @Test
     public void sampleIntegrationTest() {
---
+++ modulithbasedapp/src/test/java/io/spring/waterlevel/wateradvisor/WaterLevelTest.java
@@ -0,0 +1,22 @@
+package io.spring.waterlevel.wateradvisor;
+
+import io.spring.waterlevel.streamdata.StreamDataService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.modulith.test.ApplicationModuleTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ApplicationModuleTest(mode= ApplicationModuleTest.BootstrapMode.DIRECT_DEPENDENCIES)
+public class WaterLevelTest {
+
+    @Autowired
+    WaterLevelService waterLevelService;
+
+    @Test
+    public void waterLevelStreamSensorTest() {
+        assertThat(waterLevelService.getStatusForStreamSensor("123")).isEqualTo("Error Obtaining results for 123");
+        assertThat(waterLevelService.getStatusForStreamSensor("02311500")).contains("WITHLACOOCHEE RIVER NEAR DADE CITY, FL");
+
+    }
+}
